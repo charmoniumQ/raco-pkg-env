00b2896f83ef142a35fa96e4f17f4f093b380efa
Samuel Grayson <sam@samgrayson.me>
2021-12-01 02:08:03 -0600

    Fix bugs

--- raco-pkg-env-lib/main.rkt
+++ raco-pkg-env-lib/main.rkt
@@ -9,35 +9,44 @@
          setup/dirs)
 
 (define (setup-env-dir key system-config env-config proc)
-  (let ([src (hash-ref system-config 'lib-dir #f)]
-        [dest (hash-ref env-config 'lib-dir)])
-    (make-directory* dest)
+  (let ([src (hash-ref system-config key #f)]
+        [dest (hash-ref env-config key)])
     (when src
       (define (mkdir n)
-        (make-directory* (simplify-path (build-path n ".."))))
+        (let
+            ([parent (simplify-path (build-path n ".."))])
+            (if (not (directory-exists? parent))
+                (begin
+                  (display (format "mkdir -p ~a~n" parent))
+                  (make-directory* parent)
+                  #t)
+                #f)))
       (define (relative-to a b #:error-if-not-contained [error-if-not-contained #f])
         (let loop
             ([a-parts (explode-path (path->complete-path a))]
              [b-parts (explode-path (path->complete-path b))])
-          (if
-           (and (not (empty? b-parts)) (equal? (first a-parts) (first b-parts)))
-           (loop (rest a-parts) (rest b-parts))
-           (if
-            (and (not (empty? b-parts)) error-if-not-contained)
-            (error (format "~a is not contained in ~a~n" a b))
-            (reroot-path b (apply build-path (append (for/list ([_ (in-range (length b-parts))]) "..") a-parts)))))))
+          (if (and (not (empty? b-parts)) (equal? (first a-parts) (first b-parts)))
+              (loop (rest a-parts) (rest b-parts))
+              (if (and (not (empty? b-parts)) error-if-not-contained)
+                  (error (format "~a is not contained in ~a~n" a b))
+                  (reroot-path b(apply build-path (append (for/list ([_ (in-range (length b-parts))]) "..") a-parts)))))))
       (define (destname n)
         (build-path dest (relative-to n src #:error-if-not-contained #t)))
       (define (link n)
         (mkdir (destname n))
+        (display (format "ln -s ~a ~a~n" n (destname n)))
         (make-file-or-directory-link n (destname n)))
       (define (copy n)
         (mkdir (destname n))
+        (display (format "cp ~a ~a~n" n (destname n)))
         (copy-file n (destname n)))
-      (for ([fname (in-directory src)]) (display (format "~a~n" fname)) (if (not (directory-exists? fname)) (proc fname copy link) '())))))
+      (for ([fname (in-directory src)])
+
+        (if (not (directory-exists? fname))
+            (proc fname copy link)
+            '())))))
 
 (define (install-environment! env-dir)
-  (define env-config-dir (build-path env-dir "etc"))
   (define system-config-rktd
     (build-path (find-config-dir) "config.rktd"))
 
@@ -81,12 +90,6 @@
                ;; 'installation-name "b1760145-c765-4f74-ac27-bd5f441c60a5"
                ))
 
-  (make-directory* env-config-dir)
-  (call-with-output-file (build-path env-config-dir "config.rktd")
-    (lambda (outp)
-      (pretty-write env-config outp)))
-
-  ;; Setup lib-dir
   ;; Copy config files, symlink everything else
   (setup-env-dir 'lib-dir system-config env-config
                  (lambda (fname copy link)
@@ -94,14 +97,17 @@
                      [#".rktd" (copy fname)]
                      [_        (link fname)])))
 
-
-  ;; Setup bin-dir
   (setup-env-dir 'bin-dir system-config env-config
                  (lambda (fname copy link) (link fname)))
 
+  (call-with-output-file (build-path env-dir "config.rktd")
+    (lambda (outp)
+      (pretty-write env-config outp)))
+
   (call-with-output-file (build-path env-dir "activate.sh")
     (lambda (outp)
-      (displayln (~a "export PLTCONFIGDIR=\"" env-config-dir "\"") outp))))
+      (display (format "export PLTCONFIGDIR=\"~a\"~n" env-dir) outp))))
+
 
 (module* main #f
   (require racket/match)
